3aee3e562cfb006ec80fb46d070d942d1182d07d
Sachin <0xsachink@gmail.com>
2023-10-30 00:18:09 +0530

    Add de_identified subject logging (#2)

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import dkim
 from dns import resolver
+import re
 import os
 
 def fetch_domain_key(domain, selector='default'):
@@ -18,3 +19,32 @@ def validate_dkim(email_raw):
     except Exception as e:
         print(f"Error during DKIM validation: {e}")
         return False
+
+def match_and_sub(text, patterns):
+    for pattern in patterns:
+        match = re.search(pattern, text)
+        if match:
+            log_text = text.replace(match.group(1), '').replace(match.group(2), '')
+            return log_text.strip()
+    return ""
+
+
+venmo_subject_patterns = [
+    r"You paid (.+?) \$(.+)",
+    r"(.+?) paid you \$(.+)",
+    r"You completed (.+?) \$(.+) charge request",
+    r"(.+?) paid your \$(.+) request",
+    r"(.+?) requests \$(.+)"
+]
+
+# Usage
+texts = [
+    "You paid Sachin $1,000",
+    "Alex Soong paid you $1,000",
+    "You completed Richard $1,000 charge request",
+    "Brian paid your $1,000 request",
+    "Richard Liang requests $1,000"
+]
+
+for text in texts:
+    print(match_and_sub(text, venmo_subject_patterns))
