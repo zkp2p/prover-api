9dd244493fc8425ec6d05e94b4027e2f65b483c6
0xSachinK <sachin@setprotocol.com>
2023-12-04 09:43:34 +0530

    Add logic to replace message id with x google message id

--- hdfc-api.py
+++ hdfc-api.py
@@ -23,16 +23,15 @@ public_values_file_path = "/root/prover-api/proofs/rapidsnark_public_[payment_ty
 # --------- VALIDATE EMAIL ------------
 
 DOMAIN = 'hdfcbank.net'
-DOMAIN_KEY_SELECTOR = 'acls01'
+DOMAIN_KEY_SELECTOR = 'acls01'  
 DOMAIN_KEY_STORED_ON_CONTRACT = 'p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnVuCsG1YQI5vudtUECLUc6nd3rwoD7vb/FZy4jQe5I5tnMIaxQ9jDMOmi0Lf9W62wpHJeZRGKgkMR6cx0voWkTnGDxKiDBajSwjP0EoIlQFTldzN7/XjXVANlHS0N4lWCEngPxmIwfCexXr6prxhjthqDeOryhJUvuMlXc8M0iYVm/Bt4aQi0bVXixBBY4NCY1YJH6ZJBKyPwOmuX'
 NAME_PATTERN = r"^[A-Z][a-z'’-]+\s([A-Z][a-z'’-]+\s?)+$"
 TEMPLATE = r"""
 """
-
 FROM_EMAIL_ADDRESS = "From: HDFC Bank InstaAlerts <alerts@hdfcbank.net>"
 EMAIL_SUBJECT = "Subject: =\?UTF-8\?q\?=E2=9D=97_You_have_done_a_UPI_txn\._Check_details\!\?="
-DOCKER_IMAGE_NAME = '0xsachink/zkp2p:modal-upi-0.1.1'
-STUB_NAME = 'zkp2p:modal-upi-0.1.1-testing'
+DOCKER_IMAGE_NAME = '0xsachink/zkp2p:modal-upi-0.1.1-testing-1'
+STUB_NAME = 'zkp2p-modal-upi-0.1.1-staging'
 
 SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
 SLACK_TOKEN = os.getenv('SLACK_TOKEN')
@@ -88,6 +87,17 @@ def alert_on_slack(error_code, email_raw_content="", log_subject=False):
     return response.status_code
 
 
+def replace_message_id_with_x_google_original_message_id(email_raw_content):
+    message_id_start = email_raw_content.find("Message-ID: <")
+    message_id_end = email_raw_content.find(">", message_id_start)
+    message_id = email_raw_content[message_id_start:message_id_end + 1]
+    x_message_id_start = email_raw_content.find("X-Google-Original-Message-ID: <")
+    x_message_id_end = email_raw_content.find(">", x_message_id_start)
+    x_message_id = email_raw_content[x_message_id_start:x_message_id_end + 1]
+    # Replace "message-id: <text>" with "message-id: <x-message-id>"
+    return email_raw_content.replace(message_id, x_message_id)
+
+
 def validate_email(email_raw_content):
 
     # validate domain key
@@ -97,11 +107,13 @@ def validate_email(email_raw_content):
         alert_on_slack(error_code)
         return False, error_code
     
+    email_raw_content = replace_message_id_with_x_google_original_message_id(email_raw_content)
+
     # Validate the DKIM signature
-    # if not validate_dkim(email_raw_content):
-    #     error_code = Error.ErrorCodes.DKIM_VALIDATION_FAILED
-    #     alert_on_slack(error_code, email_raw_content)
-    #     return False, error_code
+    if not validate_dkim(email_raw_content):
+        error_code = Error.ErrorCodes.DKIM_VALIDATION_FAILED
+        alert_on_slack(error_code, email_raw_content)
+        return False, error_code
 
     # Ensure the email is from the domain
     if not re.search(fr'{FROM_EMAIL_ADDRESS}', email_raw_content):
@@ -305,8 +317,8 @@ TEST_PAYMENT_TYPE = os.getenv("TEST_PAYMENT_TYPE")
 TEST_CIRCUIT_TYPE = os.getenv("TEST_CIRCUIT_TYPE")
 TEST_EMAIL_PATH = os.getenv("TEST_EMAIL_PATH")
 MODAL_ENDPOINT = os.getenv("MODAL_ENDPOINT")
-TEST_LOCAL_RUN = int(os.getenv("TEST_LOCAL_RUN"))
-TEST_ENDPOINT = int(os.getenv("TEST_ENDPOINT"))
+TEST_LOCAL_RUN = False
+TEST_ENDPOINT = True
 
 # confirm only one test is true
 if TEST_LOCAL_RUN + TEST_ENDPOINT != 1:
@@ -325,6 +337,10 @@ if __name__ == "__main__":
         "email": email,
         "intent_hash": "12345"
     }
+    print(
+        "Payment Type: ", email_data['payment_type'], 
+        "Circuit Type: ", email_data['circuit_type']
+    )
 
     if TEST_LOCAL_RUN:
         # Call the prove_email function
