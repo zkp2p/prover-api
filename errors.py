444b709e398888e75c034798da94bfece6f18b4d
0xSachinK <sachin@setprotocol.com>
2023-12-13 18:45:59 +0530

    Refactor hdc_api to utils and errors

---
+++ errors.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+class Errors:
+
+    class ErrorCodes(Enum):
+        INVALID_PAYMENT_TYPE = 1
+        INVALID_CIRCUIT_TYPE = 2
+        INVALID_DOMAIN_KEY = 3
+        DKIM_VALIDATION_FAILED = 4
+        INVALID_FROM_ADDRESS = 5
+        INVALID_EMAIL_SUBJECT = 6
+        PROOF_GEN_FAILED = 7
+
+    def __init__(self):
+        self.error_messages = {
+            self.ErrorCodes.INVALID_PAYMENT_TYPE: "Invalid payment type. Payment type should be venmo or hdfc",
+            self.ErrorCodes.INVALID_CIRCUIT_TYPE: "Invalid circuit type. Circuit type should be send or registration",
+            self.ErrorCodes.INVALID_DOMAIN_KEY: "❗️Domain key might have changed❗️",
+            self.ErrorCodes.DKIM_VALIDATION_FAILED: "DKIM validation failed",
+            self.ErrorCodes.INVALID_FROM_ADDRESS: "Invalid from address",
+            self.ErrorCodes.INVALID_EMAIL_SUBJECT: "Invalid email subject",
+            self.ErrorCodes.PROOF_GEN_FAILED: "Proof generation failed"
+        }
+
+    def get_error_message(self, error_code):
+        return self.error_messages[error_code]
+    
+    def get_error_response(self, error_code):
+        return {
+            "code": error_code.value,
+            "message": self.get_error_message(error_code)
+        }
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ from dns import resolver
 import re
 import hashlib
 import requests
+import os
+from dotenv import load_dotenv
 
 def fetch_domain_key(domain, selector='default'):
     try:
@@ -50,6 +52,103 @@ def upload_file_to_slack(channels, token, initial_comment, file_content):
         files=file_content
     )
     return response
+
+# --------- AWS HELPER FUNCTIONS ------------
+
+incoming_eml_file_path = "/root/prover-api/received_eml/[payment_type]_[circuit_type]_[nonce].eml"
+proof_file_path = "/root/prover-api/proofs/rapidsnark_proof_[payment_type]_[circuit_type]_[nonce].json"
+public_values_file_path = "/root/prover-api/proofs/rapidsnark_public_[payment_type]_[circuit_type]_[nonce].json"
+
+
+def write_file_to_local(file_contents, payment_type, circuit_type, nonce):
+    file_path = incoming_eml_file_path\
+        .replace("[payment_type]", payment_type)\
+        .replace("[circuit_type]", circuit_type)\
+        .replace("[nonce]", nonce)
+    with open(file_path, 'w') as file:
+        file.write(file_contents)
+    return file_path
+
+
+def read_proof_from_local(payment_type, circuit_type, nonce):
+    proof = ""
+    public_values = ""
+    proof_file_name = proof_file_path\
+        .replace("[payment_type]", payment_type)\
+        .replace("[circuit_type]", circuit_type)\
+        .replace("[nonce]", nonce)
+    public_values_file_name = public_values_file_path\
+        .replace("[payment_type]", payment_type)\
+        .replace("[circuit_type]", circuit_type)\
+        .replace("[nonce]", nonce)
+
+    # check if the file exists
+    if not os.path.isfile(proof_file_name) or not os.path.isfile(public_values_file_name):
+        print("Proof file does not exist")
+        return proof, public_values
+
+    with open(proof_file_name, 'r') as file:
+        proof = file.read()
+    
+    with open(public_values_file_name, 'r') as file:
+        public_values = file.read()
+    
+    return proof, public_values
+
+
+def prove_email(payment_type:str, circuit_type:str, nonce: str, intent_hash: str, c_witness_gen: str):
+    print('Running prove email')
+    
+    import subprocess 
+
+    # Run the circom proofgen script
+    result = subprocess.run(
+        [
+            '/root/prover-api/circom_proofgen.sh', 
+            payment_type, 
+            circuit_type, 
+            nonce, intent_hash, 
+            c_witness_gen
+        ],
+        capture_output=True, 
+        text=True
+    )
+    print(result.stdout)
+    
+    # Read the proof and public values 
+    proof, public_values = read_proof_from_local(payment_type, circuit_type, nonce)
+    return proof, public_values
+
+def read_env_credentials():
+    env_example_path = "./.env.example"
+    env_credentials = {}
+    if os.path.isfile(env_example_path):
+        def get_variable_names_from_env_file(file_path=env_example_path):
+            variable_names = []
+            with open(file_path) as file:
+                for line in file:
+                    # Ignore comments and empty lines
+                    if line.startswith('#') or line.strip() == '':
+                        continue
+                    # Extract variable name (part before the '=' character)
+                    variable_name = line.split('=')[0].strip()
+                    variable_names.append(variable_name)
+            return variable_names
+
+        # Load additional environment variables from the .env file
+        additional_vars = get_variable_names_from_env_file()
+        load_dotenv()  # Load environment variables from .env file
+        for var_name in additional_vars:
+            # If it doesnt start with local
+            if not var_name.startswith("LOCAL_"):
+                var_value = os.getenv(var_name)
+                if var_value is not None:
+                    # TODO: Make this cleaner; remove all uses of a non local/modal path env var
+                    env_credentials[var_name] = var_value
+                    var_name = var_name.replace("MODAL_", "")
+                    env_credentials[var_name] = var_value
+    return env_credentials
+    
 
 def replace_message_id_with_x_google_original_message_id(email_raw_content):
     message_id_label = "Message-ID: <"
