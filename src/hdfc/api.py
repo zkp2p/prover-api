96ff7fc8892e3fd09f0817cf572f7ed22a45d78e
0xSachinK <0xsachink@gmail.com>
2024-01-15 13:47:08 +0530

    Remove validate domain key check from hdfc

--- src/hdfc/api.py
+++ src/hdfc/api.py
@@ -73,12 +73,13 @@ def validate_email(email_raw_content):
         return False, error_code
     
     # Validate domain key
-    domain_key = fetch_domain_key(DOMAIN, selector)
-    print("Fetched domain_key", domain_key)
-    if domain_key == "" or domain_key is None or domain_key != DOMAIN_KEYS[SELECTORS.index(selector)]['key']:
-        error_code = Error.ErrorCodes.INVALID_DOMAIN_KEY
-        alert_on_slack(error_code, email_raw_content)
-        return False, error_code
+    # DNS query takes time and slows down the modal. Since this check is already done on the client, we can skip it.
+    # domain_key = fetch_domain_key(DOMAIN, selector)
+    # print("Fetched domain_key", domain_key)
+    # if domain_key == "" or domain_key is None or domain_key != DOMAIN_KEYS[SELECTORS.index(selector)]['key']:
+    #     error_code = Error.ErrorCodes.INVALID_DOMAIN_KEY
+    #     alert_on_slack(error_code, email_raw_content)
+    #     return False, error_code
     
     email_raw_content = replace_message_id_with_x_google_original_message_id(email_raw_content)
 
