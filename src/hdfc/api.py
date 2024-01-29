4e91d17b47e432a655521cc64275f2af4bec7392
0xSachinK <0xsachink@gmail.com>
2024-01-30 00:32:51 +0530

    Deploy webauthn hdfc endpoint

--- src/hdfc/api.py
+++ src/hdfc/api.py
@@ -35,7 +35,7 @@ SELECTORS = [dk['selector'] for dk in DOMAIN_KEYS]
 FROM_EMAIL_ADDRESS = "From: HDFC Bank InstaAlerts <alerts@hdfcbank.net>"
 EMAIL_SUBJECT = "Subject: =\?UTF-8\?q\?=E2=9D=97_You_have_done_a_UPI_txn\._Check_details\!\?="
 DOCKER_IMAGE_NAME = '0xsachink/zkp2p:modal-upi-0.2.2'
-STUB_NAME = 'zkp2p-modal-upi-0.2.2'
+STUB_NAME = 'zkp2p-webauth-upi-prover-0.2.2'
 DEEPVUE_BASE_URL = "https://production.deepvue.tech/v1"
 
 SLACK_TOKEN = os.getenv('SLACK_TOKEN')
