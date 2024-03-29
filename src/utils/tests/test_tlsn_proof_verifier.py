a9f45032594f424bd95a7326f83550f34a8c2fb8
Richard Liang <richardliang2015@u.northwestern.edu>
2024-03-29 21:41:21 +0800

    add 2 matches case

--- src/utils/tests/test_tlsn_proof_verifier.py
+++ src/utils/tests/test_tlsn_proof_verifier.py
@@ -12,6 +12,7 @@ load_dotenv(dotenv_path)
 os.environ['VERIFIER_PRIVATE_KEY'] = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
 @pytest.mark.parametrize("inputs, expected_output", [
+    # When both regexes pass
     ({
         "payment_type": "wise",
         "circuit_type": "transfer",
@@ -34,6 +35,7 @@ os.environ['VERIFIER_PRIVATE_KEY'] = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478
         "valid": True,
         "error_code": ""
     }),
+    # When one of the regexes doesn't pass
     ({
         "payment_type": "wise",
         "circuit_type": "transfer",
@@ -55,6 +57,29 @@ os.environ['VERIFIER_PRIVATE_KEY'] = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478
         "public_values": [],
         "valid": False,
         "error_code": 11
+    }),
+    # When 2 matches
+    ({
+        "payment_type": "wise",
+        "circuit_type": "transfer",
+        "regex_patterns_map": {
+            "transfer": [
+                r'"id":(\d+)',
+                r'"targetCurrency":"([A-Z]{3})"'
+            ]
+        },
+        "regex_target_types": {
+            "transfer": ["string", "string"]
+        },
+        "error_codes_map": {
+            "transfer": 11
+        },
+        "send_data": 'AAAAAA,"targetCurrency":"EUR",XXXXXX',
+        "recv_data": 'AAAAAA,"id":41213881,XXXXXXAAAAAA,"id":41213881,XXXXXX'  
+    }, {
+        "public_values": ["41213881", "EUR"],
+        "valid": True,
+        "error_code": ""
     })
 ])
 def test_verify_extracted_regexes(inputs, expected_output):
