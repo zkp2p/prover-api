297779328ecdc91990acb087ad78680276beff19
0xSachinK <0xsachink@gmail.com>
2024-09-20 13:04:29 +0530

    Enable c witness gen

--- src/namecheap/api.py
+++ src/namecheap/api.py
@@ -27,8 +27,8 @@ SELECTORS = [dk['selector'] for dk in DOMAIN_KEYS]
 # NAME_PATTERN = r"^[A-Z][a-z'’-]+\s([A-Z][a-z'’-]+\s?)+$"
 FROM_EMAIL_ADDRESS = 'From: "NameCheap.com Support" <support@namecheap.com>'
 EMAIL_SUBJECT = 'Subject: PUSH DOMAIN CONFIRMATION EMAIL - Namecheap.com'
-DOCKER_IMAGE_NAME = '0xsachink/zkp2p:modal-namecheap-staging-0.2.6-v4'
-STUB_NAME = 'zkp2p-modal-namecheap-staging-0.2.6'
+DOCKER_IMAGE_NAME = '0xsachink/zkp2p:modal-namecheap-staging-0.2.7-v1'
+STUB_NAME = 'zkp2p-modal-namecheap-staging-0.2.7'
 
 SLACK_TOKEN = os.getenv('SLACK_TOKEN')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
